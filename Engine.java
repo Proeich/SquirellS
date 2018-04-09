fcd572fd81a221654f77ee3e990a5bfc28ef3ce8
eich <mirbaha@web.de>
2018-04-09 15:24:15 +0200

    Neuer Testeintrag

---
+++ Engine.java
@@ -0,0 +1,56 @@
+package PROG2_SS2018.Aufgabe2;
+
+import PROG2_SS2018.Aufgabe2.Entities.Environment.*;
+import PROG2_SS2018.Aufgabe2.Entities.Player.*;
+import PROG2_SS2018.Aufgabe2.Utils.*;
+
+public class Engine {
+
+
+    //Test von 15.22
+
+    public static void main(String[] args){
+        Engine gg = new Engine(
+                new Entity[]{new GoodBeast(),
+                new BadBeast(),
+                new MasterSquirell(1)}
+                ){};
+
+        gg.run();
+
+    }
+
+    private EntitySet set;
+
+    public Engine(){}
+
+    public Engine(Entity[] in){
+        set = new EntitySet();
+        for(Entity en : in){
+            set.insertIn(en);
+        }
+
+    }
+
+    public void run(){
+        while(true){
+            processInput();
+            render();
+            update();
+        }
+    }
+
+    private void update(){}
+
+    public void render(){
+        System.out.println(set.toString());
+    }
+
+    private void processInput(){
+        set.superStep();
+    }
+
+
+
+
+}
