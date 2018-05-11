315effa14e4ef9fbd1bf2d6f247f3e87438741d2
eich <mirbaha@web.de>
2018-05-11 11:14:44 +0200

    Aufgabe 4 Commands eingebunden

---
+++ Utils/Command/CommandScanner.java
@@ -0,0 +1,80 @@
+package Utils.Command;
+
+import Utils.EntitySet;
+import Utils.UI;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+
+public class CommandScanner implements UI {
+
+    private final Commands commands;
+    private final EntitySet entitySet;
+
+    public CommandScanner(EntitySet entitySet){
+        this.entitySet = entitySet;
+        commands = new Commands(entitySet);
+    }
+
+    private String[] read() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String a = bf.readLine();
+        String[] buf = a.split(" ");
+        return buf;
+    }
+
+    public boolean execCommand(String command){
+        try {
+            Method method = commands.getClass().getMethod(command);
+            Object reval = method.invoke(commands);
+
+            if(reval == null){
+                return true;
+            }
+            return (Boolean)reval;
+        }catch (NoSuchMethodException e1){
+            System.out.println("No Such Utils.Command");
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean execCommand(String command, Object Arguments){
+        try {
+            Method method = commands.getClass().getMethod(command, Arguments.getClass());
+            Object reval = method.invoke(commands, Arguments);
+            return true;
+        }catch (NoSuchMethodException e1){
+            System.out.println("No Such Utils.Command");
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public void getCommand() {
+        while(true){
+            try {
+                System.out.print("Bitte geben sie einen Befehl ein!" + '\n' + "<help> für alle Befehle!"+'\n');
+                String[] p = read();
+                if(p.length < 2){
+                    if(execCommand(p[0])){
+                        break;
+                    }
+                }else{
+                    if(execCommand(p[0],p[1])){
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Utils/Command/Commands.java
@@ -0,0 +1,96 @@
+package Utils.Command;
+
+import Entities.Environment.Entity;
+import Entities.Environment.EntityType;
+import Utils.EntitySet;
+import Utils.Vector2;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Commands {
+
+    private final EntitySet entitySet;
+
+    public Commands(EntitySet entitySet){
+        this.entitySet = entitySet;
+    }
+
+    public void writeShit(String s){
+        System.out.println("(\"Hallo\")" + s);
+    }
+
+    public void writeShitN(String[] s){
+        System.out.println("(\"Hallo\")" + s);
+    }
+
+    public void down(){
+        for(Entity e : entitySet.getSet()){
+            if(e.getType() == EntityType.MasterSquirrel){
+                (e).move(new Vector2(1, 0));
+            }
+        }
+    }
+    public void up(){
+        for(Entity e : entitySet.getSet()){
+            if(e.getType() == EntityType.MasterSquirrel){
+                (e).move(new Vector2(-1, 0));
+            }
+        }
+    }
+
+    public void left(){
+        for(Entity e : entitySet.getSet()){
+            if(e.getType() == EntityType.MasterSquirrel){
+                (e).move(new Vector2(0, -1));
+            }
+        }
+    }
+
+    public void right(){
+        for(Entity e : entitySet.getSet()){
+            if(e.getType() == EntityType.MasterSquirrel){
+                (e).move(new Vector2(0, 1));
+            }
+        }
+    }
+
+    public void exit(){
+        System.exit(42);
+    }
+
+    public boolean masterEnergy(){
+        for(Entity e : entitySet.getSet()){
+            if(e.getType() == EntityType.MasterSquirrel){
+                System.out.println("MasterSquirellEnergie = " + e.getEnergie());
+            }
+        }
+        return false;
+    }
+
+    public boolean help(){
+
+        List<String> lis = new ArrayList<>();
+
+        Class<?> e1 = this.getClass();
+
+        for(Method method : e1.getDeclaredMethods()){
+            StringBuilder sb = new StringBuilder();
+
+            for(Parameter parameter : method.getParameters()){
+                sb.append(parameter.getType().getClass().getName()).append(", ");
+            }
+            lis.add(method.getName() + " Args: (" + sb.toString() + ")");
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String s : lis){
+            sb.append(s).append('\n');
+        }
+        System.out.println(sb.toString());
+        return false;
+    }
+
+
+}
